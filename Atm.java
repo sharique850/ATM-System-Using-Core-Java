c3179ae2aef84b6a19f262317e9d593d48c5bae9
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2022-12-14 14:13:57 +0530

    Add files via upload
    ATM.java File Uploaded

---
+++ Atm.java
@@ -0,0 +1,31 @@
+<div id="nav">
+			<a class="toplink">Gmail</a>
+			<a class="toplink">Image</a>
+				<a class="toplink"></a>
+		</div>
+		
+		
+#nav {
+	display: flex;
+	justify-content: flex-end;
+	margin-top: 15px;
+}
+
+
+#nav a {
+	margin-right: 20px;
+}
+
+#nav a.topimage {
+	width:20px;
+	height:20px;
+	background:red;
+	border-radius:50px;
+}
+
+
+
+#footer-content {
+	background: #eee;
+	border-top: 1px solid black; 
+}
